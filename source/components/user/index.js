c6d4047d0b1ba304a528a737006e1ec9809e834d
Sergio Marin <higher.vnf@gmail.com>
2016-05-27 18:43:18 -0400

    Force users profile on tab click

--- source/components/user/index.js
+++ source/components/user/index.js
@@ -2,8 +2,8 @@ export default function UserTabsCtrlFactory (ngComponent) {
   ngComponent.controller('UserTabsCtrl', UserTabsCtrl)
 
   function UserTabsCtrl ($scope, $state) {
-    this.go = (where, conf = { reload: true, location: true }) => {
-      $state.go(where, conf)
+    this.go = (where, vars = {}, conf = { reload: true, location: true }) => {
+      $state.go(where, vars, conf)
     }
   }
 } 
