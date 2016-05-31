113ef4947509f36372a5491736280e9bd4e95a8f
Sergio Marin <higher.vnf@gmail.com>
2016-05-31 15:00:28 -0400

    Display add account information and no earnings details

--- source/components/balance/index.js
+++ source/components/balance/index.js
@@ -1,6 +1,13 @@
 export default function balanceCtrlFactory (ngComponent) {
   ngComponent.controller('balanceCtrl', balanceCtrl)
 
-  function balanceCtrl () {
+  function balanceCtrl (currentUser) {
+    var _ = this
+    _.user = currentUser
+
+    
+    function hasBankInformation() {
+      return _.user.attributes.bank_account !== null && typeof _.user.attributes.bank_account === 'object'
+    }
   }
 }
