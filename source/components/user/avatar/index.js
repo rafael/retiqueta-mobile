3f63c35d5518419dc84e642a8d90497a05862932
Sergio Marin <higher.vnf@gmail.com>
2016-06-06 19:07:36 -0400

    Resolve bug where dont show user avatar when has one defined

--- source/components/user/avatar/index.js
+++ source/components/user/avatar/index.js
@@ -6,17 +6,20 @@ export default function userAvatarFactory (ngComponent) {
       templateUrl: 'user/avatar/index.html',
       restrict: 'E',
       scope: {
-        src: '@'
+        src: '='
       },
-      link: function preLink(scope, iElement, iAttrs, controller) { 
-        scope.image = () => {
-          return (hasPicture(scope.src)) ? scope.src : 'images/guess_user.gif'
-        }
-        
-        function hasPicture (source) {
-          return  source !== ''
-          && source !== '/pics/original/missing.png'
-        }
+      link: preLink    
+    }
+
+    function preLink(scope, iElement, iAttrs, controller) { 
+      scope.image = () => {
+        return (hasPicture(scope.src)) ? scope.src : 'images/guess_user.gif'
+      }
+
+      function hasPicture (source) {
+        return  typeof source !== 'undefined'
+        && source !== ''
+        && source !== '/pics/original/missing.png'
       }
     }
   }
