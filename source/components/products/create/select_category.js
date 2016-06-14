03b1a3641961f05ce288824d0b62eea4bf278ea9
Sergio Marin <higher.vnf@gmail.com>
2016-06-14 15:41:44 -0400

    Several small changes from trello
    - Add translation to Fellowship view title (following or followers)
    - Resolve bug with description cache after select category. (you lose the description after select category)
    - Add posibility to select on product creation the same category and return to product creation view
    - Update the Add product picture image
    - Remove the success notification when user update profile

--- source/components/products/create/select_category.js
+++ source/components/products/create/select_category.js
@@ -3,18 +3,17 @@ import { Rules as FormRules, baseErrorsObject } from './product_form_fields'
 export default function ProductSelectionFactory (ngComponent) {
   ngComponent.controller('SelectCategoryCtrl', SelectCategoryCtrl)
 
-  function SelectCategoryCtrl ($state, ProductStore, ENV) {
+  function SelectCategoryCtrl ($state, ProductStore, ENV, $scope) {
     var _ = this
     _.product = ProductStore.get()
     _.validationRules = FormRules
-
-    _.onChangeValueCallBack = () => {
-      ProductStore.set(_.product)
+    _.onChangeValueCallBack = (category) => {
+      ProductStore.set(Object.assign(_.product, {category: category}))
       $state.go('productsNew', {}, { location: 'replace', reload: true })
     }
 
-    Object.observe(_.product, changes => {
-      changes.forEach(_.onChangeValueCallBack)
+    $scope.$on("$ionicView.enter", (event, data) => {
+      _.product = ProductStore.get()
     })
   }
 }
