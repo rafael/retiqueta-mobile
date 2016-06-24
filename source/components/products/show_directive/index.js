f1f9d541df18133002152c8657f4f1c38ba21ced
Sergio Marin <higher.vnf@gmail.com>
2016-06-23 20:16:27 -0400

    Add marker if product is sold (#71)

--- source/components/products/show_directive/index.js
+++ source/components/products/show_directive/index.js
@@ -18,10 +18,25 @@ export default function ProductDirective (ngComponent) {
         scope.product.relationships.product_pictures = scope.product.relationships.product_pictures.slice(0, 1)
       }
       scope.detail = attrs.detail || false
-      scope.isCurrentUser = () => {
+      scope.isCurrentUser = isCurrentUser
+      scope.toggleComment = toggleComment
+      scope.canBuy = canBuy
+
+      // scope.product.attributes.status = "sold"
+
+      function canBuy () {
+        return !isSold() && !isCurrentUser()
+      }
+
+      function isSold () {
+        return scope.product.attributes.status === "sold"
+      }
+        
+      function isCurrentUser () {
         return scope.product.relationships.user.id === scope.currentUser.id
       }
-      scope.toggleComment = () => {
+        
+      function toggleComment () {
         scope.commentToggleFunc.call()
       }
     }
