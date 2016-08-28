0d3273134e84e64d5bbdee22606ec60466c60dab
Sergio Marin <higher.vnf@gmail.com>
2016-08-28 20:16:57 -0300

    Dont guess if methodId is already there

--- source/components/checkout/creditcard_token/index.js
+++ source/components/checkout/creditcard_token/index.js
@@ -7,7 +7,8 @@ const creditCardObj = {
   cardExpirationMonth: '',
   cardExpirationYear: '',
   cardholderName: '',
-  docNumber: ''
+  docNumber: '',
+  methodID: ''
 }
 
 export default function CreditCardTokenFactory (ngComponent) {
@@ -93,7 +94,7 @@ export default function CreditCardTokenFactory (ngComponent) {
           })
 
           function changeCredictCard () {
-            if (scope.creditcard.cardNumber.length >= 10) {
+            if (scope.creditcard.cardNumber.length >= 10 && scope.creditcard.methodID !== '') {
               return MercadopagoFactory.guessPaymentMethod(scope.creditcard.cardNumber)
               .then(onGuess)
               .catch(errorOnGuess)
