62c854e6eae38a5c69e64b46f2f2bf1c4f181093
Whitney Zack <wcz8292@gmail.com>
2016-11-01 19:40:34 -0700

    Infinite scroll and better predictive loading

--- source/components/search/index.js
+++ source/components/search/index.js
@@ -11,92 +11,54 @@ export default function searchFactory (ngComponent) {
     _.noResult = false
     _.loading = false
     _.page = 1
-    _.canLoadMore = true
     _.pageSize = PAGE_SIZE
 
     // Search function
     _.search = searchProducts
-    _.loadMore = loadMore
     _.clear = clear
 
     function LoadProduct () {
+      _.text = ''
       if ($stateParams.hasOwnProperty('word') && typeof $stateParams.word !== 'undefined' && $stateParams.word !== '') {
         _.text = $stateParams.word
         searchProducts()
-      } else if (_.text !== '') {
-        searchProducts()
       } else {
-        prePopulate()
+        searchProducts()
       }
     }
 
     function clear () {
       _.text = ''
-      prePopulate()
+      searchProducts()
     }
 
-    function prePopulate (page = 1, add = false) {
-      if (ENV.isProduction()) {
-        facebookConnectPlugin.logEvent('search request');
-      }
-      return Product.getFeatured({
-        include: 'product_pictures',
-        'page[number]': page,
-        'page[size]': PAGE_SIZE,
-      }).then((result) => {
-        return setProducts(result, add)
-      })
-      .catch((error) => {
-        if (ENV.isProduction()) {
-          facebookConnectPlugin.logEvent('search request error')
-        }
-        Utils.swalError(error)
-      })
-      .finally(() => {
-        if (page === 1) {
-          $ionicScrollDelegate.scrollTop()
-        }
-      })
-    }
-
-    function loadMore (nextpage) {
-      if (_.text !== '') {
-        return populateWithProduct(nextpage, true)
-      } else {
-        return prePopulate(nextpage, true)
-      }
-    }
-
-    function searchProducts (page = 1) {
-      if (_.text === '') { return }
-      _.noResult = false
-      _.loading = true
-      try {
-        $cordovaKeyboard.close()
-      } catch(e) {}
-      console.log('Searching: ', _.text)
-      populateWithProduct(page)
-    }
-
-    function populateWithProduct (page, add = false) {
+    function searchProducts (page = 1, add = false) {
+      console.log('Search request: page->' + page + ' add-> ' + add + ' text->' + _.text)
       if (lastSearch !== _.text) {
-        Utils.logger.info('Diferent search, reset page and canLoadMore')
-        _.canLoadMore = true
+        Utils.logger.info('Different search, reset page')
         _.page = 1
         page = 1
       }
+
       if (ENV.isProduction()) {
         facebookConnectPlugin.logEvent('search nextpage request')
       }
-      if (page === 1) {
-        $rootScope.$broadcast('loading:show')
-      }
-      return Product.search({
-        q: _.text,
+
+      _.noResult = false
+      _.loading = true
+
+      try {
+        $cordovaKeyboard.close()
+      } catch(e) {}
+
+      var searchRequest = {
+        'q': _.text,
         'page[number]': page,
         'page[size]': PAGE_SIZE,
-        include: 'user,product_pictures'
-      }).then((result) => {
+        'include': 'user,product_pictures'
+      }
+
+      return Product.search(searchRequest).then((result) => {
         lastSearch = angular.copy(_.text)
         return setProducts(result, add)
       })
@@ -107,11 +69,13 @@ export default function searchFactory (ngComponent) {
         Utils.swalError(error)
       })
       .finally(() => {
-        _.loading = false
         console.log('Search complete')
         if (page === 1) {
           $ionicScrollDelegate.scrollTop()
         }
+        _.loading = false
+        _.page = page+1
+        $scope.$broadcast('scroll.infiniteScrollComplete');
       })
     }
 
@@ -129,8 +93,7 @@ export default function searchFactory (ngComponent) {
       changes.forEach((change) => {
         if (change.name === 'text' && _.text === '') {
           _.page = 1
-          _.canLoadMore = true
-          prePopulate()
+          searchProducts()
         }
       })
     })
@@ -139,7 +102,6 @@ export default function searchFactory (ngComponent) {
       if (ENV.isProduction()) {
         facebookConnectPlugin.logEvent('search load');
       }
-      _.canLoadMore = true
     })
     LoadProduct()
   }
