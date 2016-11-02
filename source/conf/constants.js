64620702e864d7d5d882a409419ade7e4c4527aa
Rafael Chacón <rafaelchacon@gmail.com>
2016-11-01 18:37:36 -0700

    Add a parameter to set api endpoint

--- source/conf/constants.js
+++ source/conf/constants.js
@@ -2,6 +2,7 @@ const DEVELOPMENT = 'development'
 const PRODUCTION = 'production'
 const node_env = process.env.NODE_ENV || DEVELOPMENT
 const DEBUG_MODE = process.env.DEBUG_MODE || true
+const API_ENDPOINT = process.env.API_ENDPOINT || 'https://api.retiqueta.com'
 
 function mercadopagoKeys () {
   switch (node_env) {
@@ -18,8 +19,7 @@ const ENV = {
   app_name: 'Retiqueta',
   type: node_env,
   api: {
-    // url: (node_env === 'production') ? 'https://api.retiqueta.com':'http://localhost:9393'
-    url: (node_env === 'production') ? 'https://api.retiqueta.com':'https://api.retiqueta.com'
+    url: (node_env === 'production') ? 'https://api.retiqueta.com': API_ENDPOINT
   },
   isDevelopment () {
     return this.type === DEVELOPMENT
