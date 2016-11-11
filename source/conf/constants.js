63fc58488f13692f5e27aaab0f718597db6cc6a1
Whitney Zack <wcz8292@gmail.com>
2016-11-10 23:05:00 -0800

    Make buttons flex size

--- source/conf/constants.js
+++ source/conf/constants.js
@@ -1,14 +1,14 @@
 const DEVELOPMENT = 'development'
 const PRODUCTION = 'production'
 const node_env = process.env.NODE_ENV || DEVELOPMENT
-const API_ENDPOINT = process.env.API_ENDPOINT || 'http://192.168.99.100'
+const API_ENDPOINT = process.env.API_ENDPOINT || 'https://api.retiqueta.com'
 const DEBUG_MODE = process.env.DEBUG_MODE || true
 
 function mercadopagoKeys () {
   switch (node_env) {
     case PRODUCTION:
       return 'APP_USR-024493bc-8221-4da7-8ff8-4fb1d96cf520'
-    default: 
+    default:
       return 'TEST-f4c0a8de-a40c-46c2-90f9-202e66994dd3'
   }
 }
