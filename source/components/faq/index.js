8db8eee04b09f4ee27b594b4f57ae1bc26bf713b
Whitney Zack <wcz8292@gmail.com>
2016-11-15 18:13:20 -0800

    Not perfectly optimized but should get the job done

--- source/components/faq/index.js
+++ source/components/faq/index.js
@@ -1,13 +1,14 @@
 export default function faqCtrlFactory (ngComponent) {
   ngComponent.controller('faqCtrl', faqCtrl)
 
-  function faqCtrl (Faqs) {
+  function faqCtrl (Faqs, $cordovaAppVersion) {
     const _ = this
     _.shownGroup = null
     _.faqs = Faqs
     _.toggleGroup = toggleGroup
     _.isGroupShown = isGroupShown
-      
+    _.info = info
+
     function toggleGroup (group) {
       if (isGroupShown(group)) {
         _.shownGroup = null
@@ -15,9 +16,23 @@ export default function faqCtrlFactory (ngComponent) {
         _.shownGroup = group
       }
     }
-    
+
     function isGroupShown (group) {
       return _.shownGroup === group;
     };
+
+    function info(item) {
+      var reVersion = /app_version/gi;
+      var rgCode = /app_code/gi;
+      var updatedItem = item
+      $cordovaAppVersion.getVersionCode().then(function (code) {
+        updatedItem = item.replace(reVersion, code);
+      });
+      $cordovaAppVersion.getVersionNumber().then(function (version) {
+        updatedItem = updatedItem.replace(rgCode, version);
+      });
+      return updatedItem
+    }
+
   }
 }
