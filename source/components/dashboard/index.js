bce0b2ca78e5411f34c58663740533de8fff7b53
Whitney Zack <wcz8292@gmail.com>
2016-11-22 09:58:06 -0800

    Add semi-colons and remove scroll to top

--- source/components/dashboard/index.js
+++ source/components/dashboard/index.js
@@ -8,47 +8,47 @@ export default function DashboardFactory (ngComponent) {
     $scope,
     ENV,
     $rootScope) {
-    var _ = this
+    var _ = this;
     var ONE_HOUR = 60 * 60;
     var ONE_DAY = 60 * 60 * 24;
-    var date = new Date
-    _.cards = []
-    _.isMore = true
-    _.loading = false
-    _.page = 1
-    _.pageSize = 5
-    _.getCards = getCards
+    var date = new Date;
+    _.cards = [];
+    _.isMore = true;
+    _.loading = false;
+    _.page = 1;
+    _.pageSize = 5;
+    _.getCards = getCards;
 
     function filterCard(card) {
-      return card.type == 'featured_picks' || card.type == 'user_likes'
+      return card.type == 'featured_picks' || card.type == 'user_likes';
     }
 
     function howOld(card) {
-      var difference = (date - new Date(card.attributes.created_at)) / 1000
-      var age = 0
+      var difference = (date - new Date(card.attributes.created_at)) / 1000;
+      var age = 0;
       if (difference < ONE_HOUR) {
-        age = Math.max(Math.round(difference / 60),1) + "m"
+        age = Math.max(Math.round(difference / 60),1) + "m";
       } else if (difference < ONE_DAY) {
-        age = Math.round(difference / 3600)
+        age = Math.round(difference / 3600);
         if(age == 1) {
-          age =  "1 hr"
+          age =  "1 hr";
         } else {
-          age =  age + " hrs"
+          age =  age + " hrs";
         }
       } else {
-        age = Math.round(difference / 86400) + " d"
+        age = Math.round(difference / 86400) + " d";
       }
-      card.attributes.age = age
+      card.attributes.age = age;
     }
 
     function getCards(page = 1, add = false) {
       if(!_.loading && _.isMore) {
-        _.loading = true
+        _.loading = true;
         if (!ENV.isProduction()) {
-          console.log('Timeline card request: page->' + page + ' add-> ' + add)
+          console.log('Timeline card request: page->' + page + ' add-> ' + add);
         }
         if (page > 1 && ENV.isProduction()) {
-          facebookConnectPlugin.logEvent('timeline request scroll')
+          facebookConnectPlugin.logEvent('timeline request scroll');
         }
         var request = {
           'page[number]': page,
@@ -56,60 +56,57 @@ export default function DashboardFactory (ngComponent) {
         }
         Timeline.get(request).then((result) => {
           if(typeof result !== 'undefined' && result.length > 0) {
-            result.map(howOld)
-            result = result.filter(filterCard)
+            result.map(howOld);
+            result = result.filter(filterCard);
             if(add) {
-              _.cards = _.cards.concat(result)
+              _.cards = _.cards.concat(result);
             } else {
-              _.cards = result
+              _.cards = result;
             }
           } else if(typeof result !== 'undefined' && result.length === 0){
-            _.isMore = false
+            _.isMore = false;
             if (ENV.isProduction()) {
               facebookConnectPlugin.logEvent('timeline request end');
             } else {
-              console.log('No more results ' + _.isMore)
+              console.log('No more results ' + _.isMore);
             }
           }
         }).catch((error) => {
           if (ENV.isProduction()) {
             facebookConnectPlugin.logEvent('timeline request error');
           } else {
-            console.log('Timeline error ' + error)
+            console.log('Timeline error ' + error);
           }
-          Utils.swalError(error)
-          _.loading = false
+          Utils.swalError(error);
+          _.loading = false;
           $scope.$broadcast('scroll.infiniteScrollComplete');
         })
         .finally(() => {
           if (ENV.isProduction()) {
-            facebookConnectPlugin.logEvent('timeline request complete')
+            facebookConnectPlugin.logEvent('timeline request complete');
           } else {
-            console.log('Timeline request complete')
-          }
-          if (page === 1) {
-            $ionicScrollDelegate.scrollTop()
+            console.log('Timeline request complete');
           }
           if(_.isMore) {
-            _.page = page + 1
+            _.page = page + 1;
           }
-          _.loading = false
+          _.loading = false;
           $scope.$broadcast('scroll.infiniteScrollComplete');
         })
       } else {
-          console.log('No more results ' + _.isMore)
+          console.log('No more results ' + _.isMore);
       }
       return _.cards
     }
 
     $scope.$on("$ionicView.enter", () => {
       if (ENV.isProduction()) {
-        facebookConnectPlugin.logEvent('timeline load')
+        facebookConnectPlugin.logEvent('timeline load');
       } else {
-        console.log('Timeline load')
+        console.log('Timeline load');
       }
-      _.isMore = true
-      getCards()
+      _.isMore = true;
+      getCards();
     })
   }
 }
